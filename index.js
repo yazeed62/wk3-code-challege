d8d5826eb1359639aa8c306362526e508bdf4844
yazeedahmed <dee3ayahmed25@gmail.com>
2023-04-03 01:02:32 +0300

    second commit

--- index.js
+++ index.js
@@ -1,161 +1,91 @@
- //const BASE_URL = 'http://localhost:3000';
-// document.addEventListener('DOMcontentLoaded',function (){
-//    fetchMovies();
-// });
-  
-// function fetchmovies (){
-//     fetch('${BASE_URL}/films',{
-//        method: 'GET',
-//         headers: {
-//         //   'content-type': 'application/json',
-//        },
-//    })
-//     .then(Response =>Response.json())
-//    .then(movie =>displayFilmDetails(movie));
-// }
- 
-// function displayFilmDetails(movie){ 
-//     const availableTicket =
-//     movie.capacity - movie.ticket_sold;
-
-//     const runtime = document.createElement("T");
-//     runtime.textContent ='Runtime:${movie.runtime}minutes';
-
-//     const FilmDetailssquarebox = document.getElementById("film-details");
-
-//     const title = document.createElement("T");
-//     title.textContent= 'title:${movie.title}';
-
-//     const showtime = document.createElement("T");
-//     showtime.textContent = 'showtime:${movie.showtime}';
-
-//     const poster = document.createElement ("img");
-//     poster.src = movie.poster;
-
-//     const availableTicketElement = document.createElement ("T");
-
-//     availableTicketElement.textContent = 'Available Tickets :${available Tickets}';
-
-//     const FilmDetailsItemSquarebox = document.createElement("div");
-//     FilmDetailsItemSquarebox.classList.add("film-details-item");
-
-    
-//   FilmDetailsItemSquarebox.appendChild(poster);
-//   FilmDetailsItemSquarebox.appendChild(title);
-//   FilmDetailsItemSquarebox.appendChild(runtime);
-//   FilmDetailsItemSquarebox.appendChild(showtime);
-//   FilmDetailsItemSquarebox.appendChild(availableTicketsElement);
-
-//   FilmDetailsItemSquarebox.appendChild(FilmDetailsItemSquarebox);
-// }
-
-
 
 const BASE_URL = 'http://localhost:3000';
+
 document.addEventListener('DOMContentLoaded', function() {
-    fetchMovies();
+  fetchMovies();
+});
+
+function fetchMovies() {
+  fetch(`${BASE_URL}/films/1`, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(movie => displayFilmDetails(movie))
+  .catch(error => console.error('Error:', error));
+}
+
+function displayFilmDetails(movie) {
+  const availableTickets = movie.capacity - movie.tickets_sold;
+
+  const runtime = document.createElement("div");
+  runtime.textContent = `Runtime: ${movie.runtime} minutes`;
+
+  const filmDetailsSquarebox = document.getElementById("film-details");
+
+  const title = document.createElement("div");
+  title.textContent = `Title: ${movie.title}`;
+
+  const showtime = document.createElement("div");
+  showtime.textContent = `Showtime: ${movie.showtime}`;
+
+  const poster = document.createElement("img");
+  poster.src = movie.poster;
+
+  const availableticketsElement = document.createElement("div");
+  availableticketsElement.classList.add("available-tickets");
+  availableticketsElement.textContent = `Available tickets: ${availableTickets}`;
+
+  const buyTicketButton = document.createElement("button");
+  buyTicketButton.classList.add("buy-ticket-button");
+  buyTicketButton.textContent = "Buy Ticket";
+
+  const filmDetailsItemSquarebox = document.createElement("div");
+  filmDetailsItemSquarebox.classList.add("film-details-item-squarebox");
+
+  filmDetailsItemSquarebox.appendChild(poster);
+  filmDetailsItemSquarebox.appendChild(title);
+  filmDetailsItemSquarebox.appendChild(runtime);
+  filmDetailsItemSquarebox.appendChild(showtime);
+  filmDetailsItemSquarebox.appendChild(availableticketsElement);
+  filmDetailsItemSquarebox.appendChild(buyTicketButton);
+  filmDetailsSquarebox.appendChild(filmDetailsItemSquarebox);
+
+  buyTicketButton.addEventListener('click', () => {
+    if (availableTickets > 0) {
+      availableTickets--;
+      availableticketsElement.textContent = `Available tickets: ${availableTickets}`;
+      if (availableTickets === 0) {
+        buyTicketButton.disabled = true;
+      }
+    }
   });
+}
 
-  function fetchMovies() {
-    fetch(`${BASE_URL}/films/1`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-      },
-    })
-    .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(movies => displayFilmDetails(movies));
-
-     // .then(response => response.json())
-    //  .then(movies => displayFilmDetails(movies));
-  }
-  
-  function displayFilmDetails(movie) {
-    console.log(movie);
-
-    const availableTickets = movie.capacity - movie.tickets_sold;
-  
-    const runtime = document.createElement("div");
-    runtime.textContent = `Runtime: ${movie.runtime} minutes`;
-  
-    const filmDetailsSquarebox = document.getElementById("film-details");
-  
-    const title = document.createElement("div");
-    title.textContent = `Title: ${movie.title}`;
-  
-    const showtime = document.createElement("div");
-    showtime.textContent = `Showtime: ${movie.showtime}`;
-  
-    const poster = document.createElement("img");
-    poster.src = movie.poster;
-  
-    const availableticketsElement = document.createElement("div");
-    availableticketsElement.textContent = `Available tickets: ${availableTickets}`;
-  
-    const filmDetailsItemSquarebox = document.createElement("div");
-    filmDetailsItemSquarebox.classList.add("film-details-item");
-  
-
-    filmDetailsItemSquarebox.appendChild(poster);
-    filmDetailsItemSquarebox.appendChild(title);
-    filmDetailsItemSquarebox.appendChild(runtime);
-    filmDetailsItemSquarebox.appendChild(showtime);
-    filmDetailsItemSquarebox.appendChild(availableticketsElement);
-    filmDetailsSquarebox.appendChild(filmDetailsItemSquarebox);
-    
-
-  }
-  
-  fetch(`${BASE_URL}/films`)
+fetch(`${BASE_URL}/films`)
   .then(response => response.json())
   .then(data => {
-    
     const filmList = document.querySelector('#films');
     data.forEach(film => {
       const listItem = document.createElement('li');
       listItem.textContent = film.title;
       listItem.classList.add('film', 'item');
       filmList.appendChild(listItem);
-
     });
   })
   .catch(error => console.error('Error:', error));
 
-  const buyTicketButton = document.createElement("button");
-  buyTicketButton.classList.add("buy-ticket-button");
-  buyTicketButton.textContent = "Buy Ticket";
-
-
-
-  const buyTicketButtons = document.querySelectorAll('.buy-ticket-button');
-  buyTicketButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const availableTicketsElement = button.closest('.film-details-item-squarebox').querySelector('.available-tickets');
-        let availableTickets = parseInt(availableTicketsElement.textContent);
-        if (availableTickets > 0) {
-            availableTickets--;
-            availableTicketsElement.textContent = availableTickets + ' tickets available';
-        }
-        if (availableTickets === 0) {
-            button.disabled = true;
-          }
-        });
-      });
 
 
 
 
 
 
-
-
-
-
-
-  
 
 
 
